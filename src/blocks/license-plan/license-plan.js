9cc28a0bce537721d28797b60897c6b870d567f8
Veronika <verbronika@gmail.com>
2020-04-07 17:04:59 +0300

    feat: finish license plan

--- src/blocks/license-plan/license-plan.js
+++ src/blocks/license-plan/license-plan.js
@@ -2,7 +2,27 @@
 $(document).ready(function () {
     $("input:radio[name=plan]").click(function() {
         var value = $(this).val();
-        $("#license-plan__num").text(value);
+        $("#license-plan__num").text("#"+value);     
+        
+        $("#quantity, input:radio[name=plan]").change(function() {
+            var quant = $("#quantity").val();
+            var cost;
+            switch (value) {
+                case '1':
+                    cost = 13;
+                  break;
+                case '2':
+                    cost = 22;
+                  break;
+                case '3':
+                    cost = 34;
+                  break;
+                default:
+                    cost = 0;
+              }
+            $("#license-plan__total_num").text(cost*quant);
+        })        
     })
+    
 
   });
